8e807578319243a273dc148a64afd5da393ed929
Octavian Jitea <octavian.jitea@yahoo.com>
2021-10-08 14:47:15 +0300

    test

--- src/test.java
+++ src/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("aaa");
         System.out.println("bbbbbb");
+        System.out.println("ba");
     }
 }
